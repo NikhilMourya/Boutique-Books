643acfee0ec844105d6d95d0a4d89af220969a14
nikhil-mourya-mahasos <nikhil@mahasos.com>
2024-07-26 00:32:16 +0530

    homepage initi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import HomePage from "./page/HomePage"
 import './App.css'
 
 function App() {
@@ -8,9 +9,10 @@ function App() {
 
   return (
     <>
-      <div className='bg-teal-400'>
+      <HomePage />
+      {/* <div className='bg-teal-400'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo border" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -27,9 +29,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
+
 
 export default App
