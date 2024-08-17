c5f81bd0d1d63aa36a528c9d85d53fe2af6073a6
Manji Coder <manjicoder@gmail.com>
2024-08-17 13:34:12 -0700

    BlogPage dummy ✅

--- src/page/BlogPage.jsx
+++ src/page/BlogPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { blogs } from '../assets/data/blogs';
 import H1 from '../components/H1';
 
@@ -7,14 +8,28 @@ export default function BlogPage() {
     <main className="flex flex-col gap-y-24 py-24 pt-36">
       <H1>Blogs</H1>
 
-      <section className="my-5 container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-y-10 md:gap-x-24 lg:px-16 items-center">
+      <section className="my-5 container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-y-10 md:gap-x-20 lg:px-16 items-center">
         {blogs.map((blog) => {
           return (
-            <div className="flex flex-col gap-5 mb-16">
-              <img src={blog.image} alt={blog.title} className="rounded-md" />
-              <h2 className="text-2xl line-clamp-2">{blog.title}</h2>
-              <p className="line-clamp-3">{blog.introduction}</p>
-            </div>
+            <Link
+              to={`/blogs/${blog.title}`}
+              className="flex flex-col text-left mb-16 rounded-md overflow-hidden shadow-md border border-primary/40 shadow-primary min-h-[400px]"
+            >
+              <img src={blog.image} alt={blog.title} className="" />
+              <div className="px-5 py-5 flex flex-col gap-y-5">
+                <h2 className="text-2xl line-clamp-2">{blog.title}</h2>
+                <p className="line-clamp-3">{blog.introduction}</p>
+
+                <div className="grid place-items-end">
+                  <Link
+                    className="px-5 py-1 border-2 bg-primary hover:bg-primary/90 transition-all border-primary rounded-full"
+                    to={`/blogs/${blog.title}`}
+                  >
+                    Read More...
+                  </Link>
+                </div>
+              </div>
+            </Link>
           );
         })}
       </section>
