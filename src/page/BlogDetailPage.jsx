799ef82b6932428385f61398159e99db89cafc3f
Manji Coder <manjicoder@gmail.com>
2024-08-17 14:28:51 -0700

    blog-detail page ✅

--- src/page/BlogDetailPage.jsx
+++ src/page/BlogDetailPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import { blogs } from '../assets/data/blogs';
 
 export default function BlogDetailsPage() {
